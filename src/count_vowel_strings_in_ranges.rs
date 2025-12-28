b6c75d0bd01636c4acc4a92d57f2e61b0920b2f1
btooom1999 <btooom1999@gmail.com>
2025-12-28 22:54:47 +0700

    optimize count_vowel_strings_in_ranges exercise

--- src/count_vowel_strings_in_ranges.rs
+++ src/count_vowel_strings_in_ranges.rs
@@ -1,22 +1,29 @@
 use std::collections::HashMap;
 
 fn vowel_strings(words: Vec<String>, queries: Vec<Vec<i32>>) -> Vec<i32> {
-    let mut hashmap = vec![0; 100_000];
+    let mut hashmap = vec![(0, false); 100_000];
     let is_vol = |b: u8| matches!(b, b'a' | b'e' | b'i' | b'u' | b'o');
     for (i, word) in words.iter().enumerate() {
         let bytes = word.as_bytes();
         if let (Some(first), Some(last)) = (bytes.first(), bytes.last()) {
+            let prev_val = if i == 0 { 0 } else { hashmap[i - 1].0 };
             if is_vol(*first) && is_vol(*last) {
-                hashmap[i] = 1;
+                hashmap[i] = (1 + prev_val, true);
             } else {
-                hashmap[i] = 0;
+                hashmap[i] = (prev_val, false);
             }
         }
     }
 
     queries
         .iter()
-        .map(|q| hashmap[q[0] as usize..=q[1] as usize].iter().sum::<i32>())
+        .map(|q| {
+            let val = hashmap[q[1] as usize].0 - hashmap[q[0] as usize].0;
+            if hashmap[q[0] as usize].1 {
+                return val + 1;
+            }
+            val
+        })
         .collect::<_>()
 }
 
