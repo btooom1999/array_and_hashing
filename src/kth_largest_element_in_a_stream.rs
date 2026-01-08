44b22f7096cc8625320f8248fb3973797767eec6
btooom1999 <btooom1999@gmail.com>
2026-01-08 13:05:45 +0700

    optimize kth_largest_element_in_a_stream exercise

--- src/kth_largest_element_in_a_stream.rs
+++ src/kth_largest_element_in_a_stream.rs
@@ -1,31 +1,31 @@
-const N: i32 = 10_000;
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 struct KthLargest {
     k: i32,
-    data: Vec<i32>
+    min_heap: BinaryHeap<Reverse<i32>>,
 }
 
 impl KthLargest {
     fn new(k: i32, nums: Vec<i32>) -> Self {
-        let mut data = vec![0; (N * 2 + 1) as usize];
+        let mut min_heap = BinaryHeap::new();
         for num in &nums {
-            data[(num + N) as usize] += 1;
-        }
-
-        Self { k, data }
-    }
-
-    fn add(&mut self, val: i32) -> i32 {
-        self.data[(val + N) as usize] += 1;
-
-        let mut count = self.k;
-        for (i, k) in self.data.iter().enumerate().rev() {
-            count -= k;
-            if count <= 0 {
-                return i as i32 - N;
+            min_heap.push(Reverse(*num));
+            if min_heap.len() as i32 > k {
+                min_heap.pop();
             }
         }
 
-        -1
+        Self { k, min_heap }
+    }
+
+    fn add(&mut self, val: i32) -> i32 {
+        self.min_heap.push(Reverse(val));
+        if (self.min_heap.len() as i32) > self.k {
+            self.min_heap.pop();
+        }
+
+        let Reverse(kth) = self.min_heap.peek().unwrap();
+        *kth
     }
 }
 
