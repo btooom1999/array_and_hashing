af604c881ca4f70d6c2ccccf706bcb65c9452c7f
btooom1999 <btooom1999@gmail.com>
2026-03-21 15:03:19 +0700

    optimize design_add_and_search_words_data_structure exercise

--- src/design_add_and_search_words_data_structure.rs
+++ src/design_add_and_search_words_data_structure.rs
@@ -1,65 +1,35 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 struct WordDictionary {
-    hashset: HashSet<String>
+    hashmap: HashMap<usize, Vec<String>>
 }
 
 impl WordDictionary {
     fn new() -> Self {
-        Self { hashset: HashSet::default() }
+        Self { hashmap: HashMap::default() }
     }
 
     fn add_word(&mut self, word: String) {
-        self.hashset.insert(word);
+        self.hashmap.entry(word.len()).or_default().push(word);
     }
 
     fn search(&self, word: String) -> bool {
-        let mut word = word.into_bytes();
-        let (mut first_dot, mut second_dot) = (-1, -1);
-        for i in 0..word.len() as i32 {
-            let char = word[i as usize];
-            if char == b'.' {
-                if first_dot != -1 {
-                    second_dot = i;
-                } else {
-                    first_dot = i;
-                }
-            }
+        if let Some(dictionary) = self.hashmap.get(&word.len()) {
+            return dictionary
+                .iter()
+                .any(|candidate| candidate
+                    .chars()
+                    .zip(word.chars())
+                    .all(|(s1, s2)| s1 == s2 || s2 == '.')
+                );
         }
 
-        if first_dot != -1 && second_dot != -1 {
-            for i in 0..26 {
-                for j in 0..26 {
-                    word[first_dot as usize] = b'a' + i;
-                    word[second_dot as usize] = b'a' + j;
-
-                    if self.hashset.contains(&String::from_utf8(word.clone()).unwrap()) {
-                        return true;
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        if first_dot != -1 {
-            for i in 0..26 {
-                word[first_dot as usize] = b'a' + i;
-
-                if self.hashset.contains(&String::from_utf8(word.clone()).unwrap()) {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        self.hashset.contains(&String::from_utf8(word).unwrap())
+        false
     }
 }
 
 pub fn main() {
     let mut word_dictionary = WordDictionary::new();
     word_dictionary.add_word("bad".to_string());
-    println!("{:?}", word_dictionary.hashset);
+    println!("{:?}", word_dictionary.hashmap);
 }
