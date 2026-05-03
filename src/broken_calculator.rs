cf03980dfb772566e1f67bee9919a31a3f85926e
btooom1999 <btooom1999@gmail.com>
2026-05-03 22:52:39 +0700

    optimize broken_calculator exercise

--- src/broken_calculator.rs
+++ src/broken_calculator.rs
@@ -1,16 +1,16 @@
 fn broken_calc(start_value: i32, mut target: i32) -> i32 {
     let mut res = 0;
-    while start_value != target {
-        if start_value > target || target % 2 == 1 {
-            target += 1;
-        } else {
+    while start_value < target {
+        if target % 2 == 0 {
             target /= 2;
+        } else {
+            target += 1;
         }
 
         res += 1;
     }
 
-    res
+    res + start_value - target
 }
 
 pub fn main() {
