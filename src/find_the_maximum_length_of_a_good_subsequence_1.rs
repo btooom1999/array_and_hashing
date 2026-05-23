2afb2b1367b71226d0f05858f1713bd94734c5d8
btooom1999 <btooom1999@gmail.com>
2026-05-23 09:40:10 +0700

    optimize find_the_maximum_length_of_a_good_subsequence_1 exercise

--- src/find_the_maximum_length_of_a_good_subsequence_1.rs
+++ src/find_the_maximum_length_of_a_good_subsequence_1.rs
@@ -1,33 +1,27 @@
-fn dfs(i: usize, k: usize, nums: &Vec<i32>, memo: &mut Vec<Vec<i32>>) -> i32 {
-    if memo[i][k] != -1 {
-        return memo[i][k];
-    }
-
-    let mut best = 1;
-    for idx in i+1..nums.len() {
-        if nums[i] == nums[idx] {
-            best = best.max(1+dfs(idx, k, nums, memo));
-        } else if k > 0 {
-            best = best.max(1+dfs(idx, k-1, nums, memo));
-        }
-    }
-
-    memo[i][k] = best;
-    best
-}
-
 fn maximum_length(nums: Vec<i32>, k: i32) -> i32 {
-    let k = k as usize;
-    let mut res = 0;
-    for i in 0..nums.len() {
-        res = res.max(dfs(i, k, &nums, &mut vec![vec![-1; k+1]; nums.len()]))
+    let (n, k) = (nums.len(), k as usize);
+    let mut dp = vec![vec![1; k+1]; n];
+    let mut res = 1;
+
+    for i in 0..n {
+        for j in 0..k+1 {
+            for prev in 0..i {
+                if nums[i] == nums[prev] {
+                    dp[i][j] = dp[i][j].max(dp[prev][j]+1);
+                } else if j > 0 {
+                    dp[i][j] = dp[i][j].max(dp[prev][j-1]+1);
+                }
+
+                res = res.max(dp[i][j]);
+            }
+        }
     }
 
     res
 }
 
 pub fn main() {
-    let nums = vec![1; 500];
-    let k = 0;
+    let nums = vec![1,2,1,1,3];
+    let k = 2;
     println!("{}", maximum_length(nums, k));
 }
