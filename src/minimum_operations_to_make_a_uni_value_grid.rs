45e0f971ad41b0d82b61d5fa483b8301b9f85315
btooom1999 <btooom1999@gmail.com>
2026-06-25 03:55:13 +0700

    optimized minimum_operations_to_make_a_uni_value_grid exercise

--- src/minimum_operations_to_make_a_uni_value_grid.rs
+++ src/minimum_operations_to_make_a_uni_value_grid.rs
@@ -2,26 +2,13 @@ fn min_operations(grid: Vec<Vec<i32>>, x: i32) -> i32 {
     let mut grid = grid.into_iter().flatten().collect::<Vec<_>>();
     grid.sort_unstable();
 
+    let remaining = grid[0] % x;
+    if grid.iter().any(|&num| num % x != remaining) {
+        return -1;
+    }
+
     let n = grid.len();
-    let mut total = 0;
-    for i in 0..n {
-        if i > 0 && grid[i] % x != grid[i-1] % x {
-            return -1;
-        }
-
-        total += grid[i];
-    }
-
-    let mut res = i32::MAX;
-    let mut prefix = 0;
-    for i in 0..n {
-        let right = (total - prefix - grid[i] - grid[i] * (n-i-1) as i32) / x;
-        let left = (grid[i] * i as i32 - prefix) / x;
-        res = res.min(left+right);
-        prefix += grid[i];
-    }
-
-    res
+    grid.iter().fold(0, |acc, &num| acc + (num - grid[n/2]).abs() / x)
 }
 
 pub fn main() {
