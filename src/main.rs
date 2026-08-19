64231312b254502bc263b750e5b4574a293436c3
btooom1999 <btooom1999@gmail.com>
2026-08-19 12:59:26 +0700

    migrate some exercises to valid folder exercise

--- src/main.rs
+++ src/main.rs
@@ -498,8 +498,7 @@ mod count_prefix_and_suffix_pairs_1;
 mod count_prefix_and_suffix_pairs_2;
 mod longest_chunked_palindrome_decomposition;
 mod distinct_echo_substrings;
-mod find_substring_with_given_hash_value;
 
 fn main() {
-    find_substring_with_given_hash_value::main();
+    distinct_echo_substrings::main();
 }
